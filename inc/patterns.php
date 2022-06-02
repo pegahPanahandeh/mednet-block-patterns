84a3109f0939371efc3f4b43fa346a2de9ab48de
= <=>
2022-06-02 11:10:36 -0700

    Added a sample block style

--- inc/patterns.php
+++ inc/patterns.php
@@ -13,19 +13,42 @@
  */
 function mednet_register_block_patterns() {
 	if ( function_exists( 'register_block_pattern' ) ) {
-		register_block_pattern(
-			'mednet-block-patterns/my-awesome-pattern',
-			array(
-				'title'       => __( 'Two buttons pattern', 'mednet-block-patterns' ),
-				'description' => ( 'Two buttons side by side' ),
-				'categories'  => array( 'MedNet' ),
-				'content'     => "<!-- wp:buttons {\"align\":\"center\"} -->\n<div class=\"wp-block-buttons aligncenter mbp-buttons\"><!-- wp:button {\"backgroundColor\":\"very-dark-gray\",\"borderRadius\":0} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-background has-very-dark-gray-background-color no-border-radius\">" . esc_html__( 'Button One', 'my-plugin' ) . "</a></div>\n<!-- /wp:button -->\n\n<!-- wp:button {\"textColor\":\"very-dark-gray\",\"borderRadius\":0,\"className\":\"is-style-outline\"} -->\n<div class=\"wp-block-button is-style-outline\"><a class=\"wp-block-button__link has-text-color has-very-dark-gray-color no-border-radius\">" . esc_html__( 'Button Two', 'my-plugin' ) . "</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons -->",
-			)
-		);
+		awesome_pattern();
+		tier2();
 	}
 
 }
 
+/**
+ * Define the patterns
+ * test pattern
+ */
+function awesome_pattern() {
+	register_block_pattern(
+		'mednet-block-patterns/my-awesome-pattern',
+		array(
+			'title'       => __( 'Two buttons pattern', 'mednet-block-patterns' ),
+			'description' => ( 'Two buttons side by side' ),
+			'categories'  => array( 'MedNet Components' ),
+			'content'     => "<!-- wp:buttons {\"align\":\"center\"} -->\n<div class=\"wp-block-buttons aligncenter mbp-buttons\"><!-- wp:button {\"backgroundColor\":\"very-dark-gray\",\"borderRadius\":0} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-background has-very-dark-gray-background-color no-border-radius\">" . esc_html__( 'Button One', 'my-plugin' ) . "</a></div>\n<!-- /wp:button -->\n\n<!-- wp:button {\"textColor\":\"very-dark-gray\",\"borderRadius\":0,\"className\":\"is-style-outline\"} -->\n<div class=\"wp-block-button is-style-outline\"><a class=\"wp-block-button__link has-text-color has-very-dark-gray-color no-border-radius\">" . esc_html__( 'Button Two', 'my-plugin' ) . "</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons -->",
+		)
+	);
+}
+/**
+ * Tier 2 section landing page
+ */
+function tier2() {
+	include 'patterns/tier-2.php';
+	register_block_pattern(
+		'mednet-block-patterns/tier-2-page',
+		array(
+			'title'       => __( 'Tier 2 page', 'mednet-block-patterns' ),
+			'description' => ( 'Section page with thumbnails, quicklinks and contacts.' ),
+			'categories'  => array( 'MedNet Pages' ),
+			'content'     => $tier2,
+		)
+	);
+}
 /**
  * Register MedNet category
  */
@@ -33,11 +56,16 @@ function mednet_register_block_categories() {
 	if ( function_exists( 'register_block_pattern_category' ) ) {
 
 		register_block_pattern_category(
-			'MedNet',
-			array( 'label' => _x( 'Mednet Block Patterns', 'Block pattern category', 'textdomain' ) )
+			'MedNet Components',
+			array( 'label' => _x( 'MedNet Components', 'Block pattern category', 'textdomain' ) )
+		);
+		register_block_pattern_category(
+			'MedNet Pages',
+			array( 'label' => _x( 'Mednet Page Patterns', 'Block pattern category', 'textdomain' ) )
 		);
 
 	}
 }
+
 add_action( 'init', 'mednet_register_block_categories' );
 add_action( 'init', 'mednet_register_block_patterns' );
---
+++ inc/patterns/tier-2.php
@@ -0,0 +1,229 @@
+<?php
+// Escape with https://www.freeformatter.com/html-escape.html
+$tier2 = '<!-- wp:paragraph {"className":"page-intro"} -->
+<p class="page-intro"></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"align":"full","className":"navigation-grid"} -->
+<div class="wp-block-group alignfull navigation-grid"><!-- wp:columns {"align":"full","style":{"color":{"text":"#000000","background":"#ffffff"}}} -->
+<div class="wp-block-columns alignfull has-text-color has-background" style="background-color:#ffffff;color:#000000"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h2 style="font-size:24px;line-height:1.3"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading -->
+<h2></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h3 style="font-size:24px;line-height:1.3"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"full","style":{"color":{"text":"#000000","background":"#ffffff"}}} -->
+<div class="wp-block-columns alignfull has-text-color has-background" style="background-color:#ffffff;color:#000000"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h2 style="font-size:24px;line-height:1.3"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading -->
+<h2></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h3 style="font-size:24px;line-height:1.3"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"className":"section-features"} -->
+<div class="wp-block-columns section-features"><!-- wp:column {"width":"33.33%","style":{"color":{"background":"#002145"}},"textColor":"white"} -->
+<div class="wp-block-column has-white-color has-text-color has-background" style="background-color:#002145;flex-basis:33.33%"><!-- wp:heading -->
+<h2></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><li><a href="#">#</a></li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%","style":{"color":{"background":"#0055b7"}},"textColor":"white"} -->
+<div class="wp-block-column has-white-color has-text-color has-background" style="background-color:#0055b7;flex-basis:66.66%"><!-- wp:heading -->
+<h2></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>sdf</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"btn"} -->
+<div class="wp-block-button btn"><a class="wp-block-button__link">Learn More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"full","style":{"color":{"text":"#000000","background":"#ffffff"}},"className":"quicklinks-row"} -->
+<div class="wp-block-columns alignfull quicklinks-row has-text-color has-background" style="background-color:#ffffff;color:#000000"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h3 style="font-size:24px;line-height:1.3"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h3 style="font-size:24px;line-height:1.3"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+<h3 style="font-size:24px;line-height:1.3"></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"className":"section-contact"} -->
+<div class="wp-block-group section-contact"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading -->
+<h2>Contact</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3} -->
+<h3>Firstname Lastname</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Title, Department</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Email: foo@bar.baz</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Phone: 123.456.7890</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>Additional Information</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->';
